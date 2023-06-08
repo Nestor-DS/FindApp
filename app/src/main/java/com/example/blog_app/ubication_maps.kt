0f1aac9316cddd5b7b0f85565a17a01dddaf4abc
Nestor Sanchez <78669277+Nestor-DS@users.noreply.github.com>
2023-06-08 09:52:16 -0600

    Sección de maps

---
+++ app/src/main/java/com/example/blog_app/ubication_maps.kt
@@ -0,0 +1,29 @@
+package com.example.blog_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class ubication_maps : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubication_maps)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        TODO("Not yet implemented")
+    }
+}
