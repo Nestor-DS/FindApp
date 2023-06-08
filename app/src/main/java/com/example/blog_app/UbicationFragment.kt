869a8d67acccb62c99d494c0f3d2957dc540e99a
Nestor Sanchez <78669277+Nestor-DS@users.noreply.github.com>
2023-06-08 11:15:34 -0600

    Maps

--- app/src/main/java/com/example/blog_app/UbicationFragment.kt
+++ app/src/main/java/com/example/blog_app/UbicationFragment.kt
@@ -5,19 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.lottie.model.Marker
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UbicationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UbicationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class UbicationFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -37,16 +40,33 @@ class UbicationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(28.3593, -16.539329)
+        val marker = MarkerOptions().position(coordinates).title("Dispositivo!")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             UbicationFragment().apply {
