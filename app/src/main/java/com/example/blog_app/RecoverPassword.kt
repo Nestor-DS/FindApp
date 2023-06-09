be6601d9f579a6ec621779b00c5e83c969611992
Nestor Sanchez <78669277+Nestor-DS@users.noreply.github.com>
2023-06-08 23:36:21 -0600

    Contraseña

--- app/src/main/java/com/example/blog_app/RecoverPassword.kt
+++ app/src/main/java/com/example/blog_app/RecoverPassword.kt
@@ -1,11 +1,20 @@
 package com.example.blog_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class RecoverPassword : AppCompatActivity() {
+
+    private val SPLASH_DURATION = 2000L // Duración de la primera animación en milisegundos
+    private val STATIC_DURATION = 1000L // Duración para que la pantalla se quede estática en milisegundos
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recover_password2)
+
+        val handler = Handler(Looper.getMainLooper())
+
     }
 }
